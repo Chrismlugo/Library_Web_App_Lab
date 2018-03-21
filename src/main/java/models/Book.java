644fe8a28197081cd1b4d59f962d076de8e41d3e
Angus Malloch <angusbass@hotmail.com>
2018-03-21 14:28:04 +0000

    Added properties and constructor to Book class.

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private Boolean onLoan;
+    private Borrower currentBorrower;
+
+    public Book(int id, String title, String author, Boolean onLoan, Borrower currentBorrower) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.onLoan = onLoan;
+        this.currentBorrower = currentBorrower;
+    }
 }
